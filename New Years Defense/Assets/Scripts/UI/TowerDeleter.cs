44dba1841262fd68a4acbd7365022058f8b146d6
Zac Olsen <zolsen@masonlive.gmu.edu>
2017-04-08 17:07:34 -0400

    trash can

--- New Years Defense/Assets/Scripts/UI/TowerDeleter.cs
+++ New Years Defense/Assets/Scripts/UI/TowerDeleter.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class TowerDeleter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
 
+	[SerializeField] private Sprite closed = null;
+	[SerializeField] private Sprite open = null;
+
+	private Image image;
 	private bool selected;
 	private BaseStats baseStats;
 
 	void Start () {
+		
 		baseStats = GameObject.Find ("Base Stats").GetComponent<BaseStats> ();
+
+		image = GetComponent<Image> ();
+		image.sprite = closed;
 	}
 
 	void Update () {
@@ -24,10 +33,14 @@ public class TowerDeleter : MonoBehaviour, IPointerEnterHandler, IPointerExitHan
 	}
 
 	public void OnPointerEnter (PointerEventData data) {
+
 		selected = true;
+		image.sprite = open;
 	}
 
 	public void OnPointerExit (PointerEventData data) {
+
 		selected = false;
+		image.sprite = closed;
 	}
 }
